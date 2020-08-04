9bbd51c50c01d2f246ddbb381ce5b4eb65af89cf
Jialin <j6shan@gmail.com>
2020-08-04 18:29:58 -0400

    Updates

---
+++ precache-manifest.21f47e665d745442a55123f3a35c3c8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17239abe25cd3bf20c8a95fe8d5b783",
+    "url": "/travelbuddy/index.html"
+  },
+  {
+    "revision": "1ccdd101009483f15c50",
+    "url": "/travelbuddy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "075eaf924b4585ea16d5",
+    "url": "/travelbuddy/static/js/2.c9cbb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travelbuddy/static/js/2.c9cbb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ccdd101009483f15c50",
+    "url": "/travelbuddy/static/js/main.06a20d4c.chunk.js"
+  },
+  {
+    "revision": "8fdb3871c557bf9d85b7",
+    "url": "/travelbuddy/static/js/runtime-main.1b4eda5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travelbuddy/static/media/logo.5d5d9eef.svg"
+  }
+]);
