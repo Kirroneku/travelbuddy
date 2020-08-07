2462632b45286d2598c01e3a9d44ba0fe38c3aa9
Jialin <j6shan@gmail.com>
2020-08-07 18:30:33 -0400

    search places

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,9 +3,31 @@ import '../CSS/App.css';
 import GoogleApiWrapper from "./MapChart";
 
 function App() {
+
+  const [location, setLocation] = useState("Macchu Picchu");
+  const [searching, setSearch] = useState(location);
+
+  const newLocation = (event) => {
+    event.preventDefault();
+    setSearch(location);
+  }
+
   return (
     <div className="App">
-      <GoogleApiWrapper location={"Machu picchu, peru"}/>
+      <div display="flex">
+        <form onSubmit={newLocation}>
+          <label>
+            Location:
+            <input type="text" id="locationInput" onChange={(event) => setLocation(event.target.value)}></input> 
+          </label>
+          <input type="submit" value="Search"/>
+
+        </form>
+        <div>
+          <GoogleApiWrapper location={searching}/>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/Components/MapChart.js
+++ src/Components/MapChart.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 // const AnyReactComponent = ({ text }) => <div>{text}</div>;
  
 const SimpleMap = ({location}) => {
+    const API_KEY = "";
      return (
         // Important! Always set the container height explicitly
         <div style={{ height: '100vh', width: '100%' }}>
@@ -14,12 +15,12 @@ const SimpleMap = ({location}) => {
                 frameborder:"0",
                 border:"0"
             }}
-            src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyAcpGLtUQg1RVxIqZ42C6SSpMoeCBwLiBY&q=" + location}
+            src={"https://www.google.com/maps/embed/v1/place?key=" + API_KEY+ "&q=" + location}
             allowfullscreen>
             </iframe>
         </div>
     );
   
-}git commit -m "some adjustments to work in google maps, might add in a on website autocorrect, probably not"
+}
  
 export default SimpleMap;
